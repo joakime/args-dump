3c003c5b002cc92611cfff53795dcf4fc8e5d869
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2023-07-05 10:04:46 -0500

    Dump properties files too

--- src/main/java/net/erdfelt/ArgsDump.java
+++ src/main/java/net/erdfelt/ArgsDump.java
@@ -1,6 +1,13 @@
 package net.erdfelt;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Properties;
 
 public class ArgsDump
 {
@@ -17,6 +24,44 @@ public class ArgsDump
         for (String arg : args)
         {
             System.out.printf("args[%d]: [%s]%n", i++, arg);
+            if (arg.endsWith(".properties"))
+            {
+                dumpProperties(arg);
+            }
+        }
+    }
+
+    private static void dumpProperties(String arg)
+    {
+        Path propPath = Path.of(arg);
+        if (!Files.exists(propPath))
+        {
+            System.out.printf(" !! Path does not exist: %s%n", propPath);
+            return;
+        }
+
+        if (!Files.isRegularFile(propPath))
+        {
+            System.out.printf(" !! Path is not a file: %s%n", propPath);
+            return;
+        }
+
+        try (InputStream inputStream = Files.newInputStream(propPath))
+        {
+            Properties props = new Properties();
+            props.load(inputStream);
+            //noinspection unchecked
+            List<String> keys = (List<String>)Collections.list(props.propertyNames());
+            Collections.sort(keys);
+            for (String key : keys)
+            {
+                System.out.printf("  > [%s]: [%s]%n", key, props.getProperty(key));
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.printf(" !! Unable to read properties: %s%n", propPath);
+            e.printStackTrace();
         }
     }
 }
